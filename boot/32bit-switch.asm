ed5cfe5ffdeda32980cf086faad17d81e51a7e92
mzwang25 <mzwang25@gmail.com>
2020-06-08 20:47:39 -0700

    update name

--- boot/32bit-switch.asm
+++ boot/32bit-switch.asm
@@ -3,12 +3,14 @@
 switch_to_pm:
     cli ; disables interrupts bc 16/32 mode handles differently
     lgdt [gdt_descriptor] ; loads GDT
-    ; set cr0 to 1
+    ; set cr0 to 1 ... cr0 is a special register so once it's updated it's in 32 bit mode
     mov eax, cr0
     or eax, 0x1
     mov cr0, eax
 
-    jmp CODE_SEG:init_pm ; far jump to clear pipeline
+    jmp CODE_SEG:init_pm ; far jump (jmps to a particular address)
+                         ; This flushes the pipeline so code won't get
+                         ; executed in the wrong mode
 
 [bits 32]
 init_pm:
